7540469727a8c97ee43c4af7a1ed1e352da2385b
Emilio P <ejpascualj@gmail.com>
2021-07-23 22:30:35 -0500

    finished coding generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,74 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `[${license} license](https://img.shields.io/badge/License-${license}-blue.svg)`;
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
+// function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `\n* [License](#license)\n`;
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License
 
-// TODO: Create a function to generate markdown for README
+    Licensed under the ${license} license.
+
+    `;
+  } else {
+    return '';
+  }
+}
+
+// function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return ` # ${data.title}
+  ${renderLicenseBadge(data.license)}
 
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.installation}
+
+  ## Usage
+  
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  
+  ${data.contribution}
+
+  ## Tests
+
+
+  ## Questions  
 `;
 }
 
